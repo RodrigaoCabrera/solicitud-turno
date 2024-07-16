fe90d44df08800c0bee4b9ccf50ed606a1f6f1a9
Rodrigo Cabrera <rodrigod33d@gmail.com>
2024-07-16 01:22:46 -0300

    ADD: Profile UI in the home

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,15 +9,15 @@ export default {
         sans: ["League Spartan Variable", ...defaultTheme.fontFamily.sans],
       },
       fontSize: {
-        xs: "0.75rem",
-        sm: "0.85rem",
-        m: "1rem",
-        lg: "1.125rem",
-        xl: "1.25rem",
-        "2xl": "1.5rem",
-        "3xl": "1.875rem",
-        "4xl": "2.25rem",
-        "5xl": "3rem",
+        xs: "0.75rem", // 12px
+        sm: "0.875rem", // 14px
+        m: "1rem", // 16px
+        lg: "1.125rem", // 18px
+        xl: "1.25rem", // 20px
+        "2xl": "1.5rem", // 24px
+        "3xl": "1.875rem", // 30px
+        "4xl": "2.25rem", // 36px
+        "5xl": "3rem", // 48px
       },
     },
   },
