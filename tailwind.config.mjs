8a2869ec367a0ec0a064f68c72bf2248a90b92a4
Rodrigo Cabrera <rodrigod33d@gmail.com>
2024-07-15 21:45:35 -0300

    ADD: Font famili and font variants

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["League Spartan Variable", ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        xs: "0.75rem",
+        sm: "0.85rem",
+        m: "1rem",
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
+        "3xl": "1.875rem",
+        "4xl": "2.25rem",
+        "5xl": "3rem",
+      },
+    },
+  },
+  plugins: [],
+};
