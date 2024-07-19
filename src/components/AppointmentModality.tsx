e95622ad2a08b16c5a4b122da0fcd1d55b0f87ab
Rodrigo Cabrera <rodrigod33d@gmail.com>
2024-07-19 00:46:57 -0300

    ADD: Style to AppointmentModality component

--- src/components/AppointmentModality.tsx
+++ src/components/AppointmentModality.tsx
@@ -6,27 +6,50 @@ interface Props {
 }
 const AppointmentModality: React.FC<Props> = ({ onChange, modality }) => {
   return (
-    <section>
-      <input
-        type="radio"
-        name="modality"
-        id="face-to-face"
-        value="face-to-face"
-        onChange={onChange}
-        defaultChecked={modality === "face-to-face"}
-      />
-      <label htmlFor="face-to-face">PRESENCIAL</label>
+    <>
+      <p className="text-sm text-[#222B45] mb-1">Modalidad</p>
+      <section className="flex w-100 gap-3">
+        <div className="flex-1">
+          <label
+            htmlFor="face-to-face"
+            className={`flex flex-col align-bottom justify-center rounded-full border-[1px] border-solid boder-[#94A3B8]  pt-1 text-center ${
+              modality === "face-to-face" && "bg-gray-100"
+            }`}
+          >
+            PRESENCIAL
+          </label>
+          <input
+            className="hidden"
+            type="radio"
+            name="modality"
+            id="face-to-face"
+            value="face-to-face"
+            onChange={onChange}
+            defaultChecked={modality === "face-to-face"}
+          />
+        </div>
 
-      <input
-        type="radio"
-        name="modality"
-        id="online"
-        value="online"
-        onChange={onChange}
-        defaultChecked={modality === "online"}
-      />
-      <label htmlFor="online">ONLINE</label>
-    </section>
+        <div className="flex-1">
+          <label
+            htmlFor="online"
+            className={`flex flex-col align-bottom justify-center rounded-full border-[1px] border-solid boder-[#94A3B8] pt-1 text-center  ${
+              modality === "online" && "bg-gray-100"
+            }`}
+          >
+            ONLINE
+          </label>
+          <input
+            className="hidden"
+            type="radio"
+            name="modality"
+            id="online"
+            value="online"
+            onChange={onChange}
+            defaultChecked={modality === "online"}
+          />
+        </div>
+      </section>
+    </>
   );
 };
 
