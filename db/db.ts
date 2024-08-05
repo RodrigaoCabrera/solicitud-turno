52590d157cfe3e79425bf17f5a7f272bd0b04722
Rodrigo Cabrera <rodrigod33d@gmail.com>
2024-08-04 21:55:38 -0300

    ADD: Config prop for db conection

--- db/db.ts
+++ db/db.ts
@@ -2,9 +2,10 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 const connectionString = import.meta.env.DATABASE_URL || "";
-
 console.log("Database Connection String:", connectionString);
 
-// Disable prefetch as it is not supported for "Transaction" pool mode
-export const client = postgres(connectionString);
+export const client = postgres(connectionString, {
+  ssl: { rejectUnauthorized: false },
+  max: 1,
+});
 export const db = drizzle(client);
