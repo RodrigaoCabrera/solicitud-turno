3fb980e349f7e467f8765f7f087fb5db6d024c2a
Rodrigo Cabrera <rodrigod33d@gmail.com>
2024-08-04 21:36:10 -0300

    ADD: Log to see DATABASE_URL

--- db/db.ts
+++ db/db.ts
@@ -3,6 +3,8 @@ import postgres from "postgres";
 
 const connectionString = import.meta.env.DATABASE_URL || "";
 
+console.log("Database Connection String:", connectionString);
+
 // Disable prefetch as it is not supported for "Transaction" pool mode
 export const client = postgres(connectionString);
 export const db = drizzle(client);
