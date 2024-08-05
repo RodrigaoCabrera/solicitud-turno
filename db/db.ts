19edd1e65637b909a67a19788ac9d4461b9ff282
Rodrigo Cabrera <rodrigod33d@gmail.com>
2024-08-04 23:11:09 -0300

    ADD: Config to db conection

--- db/db.ts
+++ db/db.ts
@@ -4,8 +4,5 @@ import postgres from "postgres";
 const connectionString = import.meta.env.DATABASE_URL || "";
 console.log("Database Connection String:", connectionString);
 
-export const client = postgres(connectionString, {
-  ssl: { rejectUnauthorized: false },
-  max: 1,
-});
+export const client = postgres(connectionString, { prepare: false });
 export const db = drizzle(client);
